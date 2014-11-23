7db3f0cdf60f3bbb8f4c5624bb43ca059edcc3b6
actis-dm <actis.dm@gmail.com>
2014-11-23 17:04:01 +0400

    Скрипт

---
+++ find.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+echo "Программа поиска файлов"
+echo "С помощью данной программы вы можете искать файл по имени или по размеру"
+echo "Разработчик: Кокорев Дмитрий"
+again=yes
+while [ "$again" = "yes" ]
+do
+echo "Поиск по имени файла или по размеру? (name/size)"
+read task
+case $task in
+name)echo "Введите каталог, в котором нужно найти файл"
+read catalog1
+if test -d /$catalog1
+then
+echo "Введите имя файла"
+read name
+if test -f $name
+then
+find /$catalog1 -name $name -print
+echo "Хотите продолжить? (yes/no)"
+read again
+else
+echo "Ошибка!Такого файла в данном каталоге не существует"
+echo "Хотите продолжить? (yes/no)"
+read again
+fi
+else
+echo "Ошибка!Такого каталога не существует"
+echo "Хотите продолжить? (yes/no)"
+read again
+fi
+;;
+size)echo "Введите каталог, в котором нужно найти файл"
+read catalog2
+if test -d /$catalog2
+then
+echo "Введите размер файла"
+read size
+find /$catalog2 -size $size -print
+echo "Хотите продолжить? (yes/no)"
+read again
+else 
+echo "Ошибка!Такого каталога не существует"
+echo "Хотите продолжить? (yes/no)"
+read again
+fi
+;;
+*)echo "Ошибка!Поиск по такому запросу не может быть осуществлен"
+echo "Хотите продолжить? (yes/no)"
+read again
+esac
+done
+echo
