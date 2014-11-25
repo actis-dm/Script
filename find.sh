999c5bcdc9c392d0ee3a72272358ad3efc6d3d28
Кокорев Дмитрий <actis.dm@gmail.com>
2014-11-25 11:46:30 +0400

    Скрипт

--- find.sh
+++ find.sh
@@ -8,46 +8,45 @@ do
 echo "Поиск по имени файла или по размеру? (name/size)"
 read task
 case $task in
-name)echo "Введите каталог, в котором нужно найти файл"
-read catalog1
-if test -d /$catalog1
-then
-echo "Введите имя файла"
-read name
-if [ -f $name ]
-then
-find /$catalog1 -name $name -print
-echo "Хотите продолжить? (yes/no)"
-read again
-else
-echo "$(tput setaf 1)Ошибка!Такого файла в данном каталоге не существует$(tput setaf 0)"
-echo "Хотите продолжить? (yes/no)"
-read again
-fi
-else
-echo "$(tput setaf 1)Ошибка!Такого каталога не существует$(tput setaf 0)"
-echo "Хотите продолжить? (yes/no)"
-read again
-fi
-;;
-size)echo "Введите каталог, в котором нужно найти файл"
-read catalog2
-if [ -d /$catalog2 ]
-then
-echo "Введите размер файла(k,b)"
-read size
-find /$catalog2 -size $size -print
-echo "Хотите продолжить? (yes/no)"
-read again
-else 
-echo "$(tput setaf 1)Ошибка!Такого каталога не существует$(tput setaf 0)"
-echo "Хотите продолжить? (yes/no)"
-read again
-fi
-;;
-*)echo "$(tput setaf 1)Ошибка!Поиск по такому запросу не может быть осуществлен$(tput setaf 0)"
-echo "Хотите продолжить? (yes/no)"
-read again
+	name)echo "Введите каталог, в котором нужно найти файл"
+		read catalog1
+		if [ -d /$catalog1 ]
+		then
+		echo "Введите имя файла"
+		read name
+			if [ -f $name ]	
+			then
+			find /$catalog1 -name $name -print
+			echo "Хотите продолжить? (yes/no)"
+			read again
+			else
+			echo "$(tput setaf 1)Ошибка!Такого файла в данном каталоге не существует$(tput setaf 0)"
+			echo "Хотите продолжить? (yes/no)"
+			read again
+			fi
+		else
+		echo "$(tput setaf 1)Ошибка!Такого каталога не существует$(tput setaf 0)"
+		echo "Хотите продолжить? (yes/no)"
+		read again
+		fi
+		;;
+	size)echo "Введите каталог, в котором нужно найти файл"
+		read catalog2
+		if [ -d /$catalog2 ]
+		then
+		echo "Введите размер файла(k,b)"			
+		read size
+		find /$catalog2 -size $size -print
+		echo "Хотите продолжить? (yes/no)"
+		read again
+		else 
+		echo "$(tput setaf 1)Ошибка!Такого каталога не существует$(tput setaf 0)"
+		echo "Хотите продолжить? (yes/no)"
+		read again
+		fi
+		;;
+	*)echo "$(tput setaf 1)Ошибка!Поиск по такому запросу не может быть осуществлен$(tput setaf 0)"
+		echo "Хотите продолжить? (yes/no)"
+		read again
 esac
 done
-echo
